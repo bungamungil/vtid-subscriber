9eedcb27ae7c47366d806ca24023eb8cdded2a5a
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-07-03 13:00:51 +0700

    Add LilyFeedKit library

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
         .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        .package(url: "https://github.com/bungamungil/lily-feed.git", from: "0.3.0"),
     ],
     targets: [
         .executableTarget(
@@ -20,7 +21,8 @@ let package = Package(
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
                 .product(name: "Leaf", package: "leaf"),
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "LilyFeedKit", package: "lily-feed")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
